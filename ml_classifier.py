46b8e826d62e596ae81d581f9baac4117cd24aab
Julio Acuña <urkonn@gmail.com>
2015-06-15 21:00:06 -0500

    adds some pep8 style

--- ml_classifier.py
+++ ml_classifier.py
@@ -1,48 +1,46 @@
 #!/usr/bin/env python
 # coding=utf-8
-from flask import Flask
-from flask import jsonify
-from flask import make_response
-from flask import request
+
+from flask import Flask, request, jsonify, make_response, abort
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.externals import joblib
 from bs4 import BeautifulSoup
 import re
 import os
 import sys
-sys.path.append( os.path.join(os.path.dirname(__file__), 'DeepLearningMovies/' ))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'DeepLearningMovies/'))
 from KaggleWord2VecUtility import KaggleWord2VecUtility
-sys.path.append( os.path.dirname(__file__) )
+sys.path.append(os.path.dirname(__file__))
 from profanity_filter import *
 from nltk.corpus import stopwords
 
 
 app = Flask(__name__)
 
-def review_words( raw_text ):
+
+def review_words(raw_text):
     review_text = BeautifulSoup(raw_text).get_text()
-    letters_only = re.sub("^(\w+)[0-9]@", " ", review_text) 
+    letters_only = re.sub("^(\w+)[0-9]@", "", review_text)
     callback = lambda pat: pat.group(0).decode('utf-8').lower()
-    iac = re.sub(u"Ă",u"í",letters_only)
-    ene = re.sub(u"Ñ",u"ñ",iac)
+    iac = re.sub(u"Ă", u"í", letters_only)
+    ene = re.sub(u"Ñ", u"ñ", iac)
     words = re.sub("(\w+)", callback, ene).split()
-    stops = set(stopwords.words("spanish")) 
-    meaningful_words = [w for w in words if not w in stops] 
-    return( u" ".join( meaningful_words )) 
+    stops = set(stopwords.words("spanish"))
+    meaningful_words = [w for w in words if w not in stops]
+    return u" ".join(meaningful_words)
 
 
 def evaluate_petition(features):
-    pipe = joblib.load('models/bow_ng6_nf9500_ne750.pkl') 
+    pipe = joblib.load('models/bow_ng6_nf9500_ne750.pkl')
     test_data_features = pipe.named_steps['vectorizer'].transform(features)
     test_data_features = test_data_features.toarray()
     result = pipe.named_steps['forest'].predict(test_data_features)
     return result
 
-    
 
 @app.route('/sac/peticiones/filtro_malas_palabras', methods=['POST'])
 def filtro_malas_palabras():
-    f = ProfanitiesFilter(my_list, replacements="-") 
+    f = ProfanitiesFilter(my_list, replacements="-")
     if not request.json or not 'descripcion' in request.json:
         abort(400)
     task = {
@@ -51,6 +49,7 @@ def filtro_malas_palabras():
     }
     profanity_score = f.profanity_score(task['descripcion'])
     return jsonify({'indice_de_groserias': profanity_score})
+
 
 @app.route('/sac/peticiones/clasificador', methods=['POST'])
 def create_task():
@@ -62,11 +61,13 @@ def create_task():
     }
     clean_test_descripciones = []
     features = review_words(task['descripcion'])
-    clean_test_descripciones.append(u" ".join(KaggleWord2VecUtility.review_to_wordlist(features, True)))
+    clean_test_descripciones.append(u" ".join(
+        KaggleWord2VecUtility.review_to_wordlist(features, True))
+        )
     myeval = evaluate_petition(clean_test_descripciones)
-    #tasks.append(task)
+    # tasks.append(task)
     return jsonify({'dependencia_sugerida': myeval[0]}), 201
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
