8b3c155a60be50e3bba96dc420ff264e7087ed3e
Jorge Martínez <j.martinezortega@gmail.com>
2015-06-18 18:43:57 +0000

    Add recommendation web service, recieves the title of a petition, and recommneds a web site with a brief description

--- ml_classifier.py
+++ ml_classifier.py
@@ -13,6 +13,7 @@ from KaggleWord2VecUtility import KaggleWord2VecUtility
 sys.path.append(os.path.dirname(__file__))
 from profanity_filter import *
 from nltk.corpus import stopwords
+import elasticsearch
 
 
 app = Flask(__name__)
@@ -37,6 +38,26 @@ def evaluate_petition(features):
     result = pipe.named_steps['forest'].predict(test_data_features)
     return result
 
+
+def get_relevant_hits(like_text):
+    index_name = "peticion"
+    doc_type = "pregunta"
+    stop_words = ["para","apoyo","una","la","el","de","en"]
+    body = {"query": { "more_like_this" : { "fields":["titulo"],
+            "like_text" : like_text, "min_term_freq" : 1, 
+            "max_query_terms" : 100, "min_doc_freq": 0,
+            "stop_words":stop_words}}}
+    es = elasticsearch.Elasticsearch()
+    mlts = es.search(index=index_name, doc_type=doc_type,body=body)
+    relevant_sugestions = []                    
+    hits = mlts.get('hits')["hits"]
+    for hit in hits:
+        if hit["_score"] >= 0.5:
+            source = hit["_source"]
+            relevant_sugestions.append({"title": source["titulo"],
+            "description": source["sugerencia"], "link": source["link"]})
+    return relevant_sugestions
+    
 
 @app.errorhandler(400)
 def bad_request(error):
@@ -85,5 +106,16 @@ def create_task():
                     'classification_id': myeval[0]}), 201
 
 
+@app.route('/recommendations', methods=['GET'])
+def get_hits():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'title': request.json['title']
+    }
+    relevant_sugestions = get_relevant_hits(task['title'])
+    return jsonify({'results': relevant_sugestions})
+    
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
