58962ff6216b786759b71092e8c07d767a481314
Rod Wilhelmy <rwilhelmy@gmail.com>
2015-06-22 18:10:30 -0500

    Join classifier and profanity filter into a chord callback
    Uses chord to run two jobs and a callback after processing ends
    1) A text classifier
    2) A profanity filter
    3) A callback to put all together in a JSON

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@
 from celery import Celery
 from sklearn.externals import joblib
 from profanity_filter import *
+import json
 
 BROKER_URL = 'redis://localhost:6379/0'
 BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 1800}  # 1/2 hour.
@@ -16,16 +17,31 @@ def evaluate_petition(features):
     pipe = joblib.load('models/bow_ng6_nf9500_ne750.pkl')
     test_data_features = pipe.named_steps['vectorizer'].transform(features)
     test_data_features = test_data_features.toarray()
+    # TODO: check API of this method, what's returning on error?
     result = pipe.named_steps['forest'].predict(test_data_features)
-    print result
-    return result
+    if len(result) > 0:
+        return result[0]
+    else:
+        return -1
 
 
 @app.task
 def catch_bad_words_in_text(text):
     f = ProfanitiesFilter(my_list, replacements="-")
+    # TODO: check API of this method, what's returning on error?
     profanity_score = f.profanity_score(text)
-    print profanity_score
     return profanity_score
+
+
+@app.task
+def build_classification_response(results):
+    profanity_score = results[1]
+    proceeds = False if profanity_score > 0 else True
+
+    response = {
+            'agency': results[0],
+            'proceeds': proceeds
+            }
+    print json.dumps(response)
 
 
