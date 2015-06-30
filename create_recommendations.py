7c0a827ebdd1fb815096e7dd506539b5301ca435
Jorge Martínez <j.martinezortega@gmail.com>
2015-06-30 01:38:54 +0000

    Delete the previous 'peticion' index in elasticsearch, and creates a new one correctly.

--- create_recommendations.py
+++ create_recommendations.py
@@ -1,6 +1,6 @@
 from elasticsearch import Elasticsearch,helpers
 es = Elasticsearch()
-
+es.indices.delete(index='peticion', ignore=[400, 404])
 dicts_from_file = []
 with open('peticion_titulo.json','r') as inf:
     for line in inf:
