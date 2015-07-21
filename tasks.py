96059c6e55f757295a6a6f0d148427f34aae2fad
Jorge Martínez <j.martinezortega@gmail.com>
2015-07-21 15:28:40 +0000

    Raise exception when endpoint fails #86

--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ from celery import Celery
 from sklearn.externals import joblib
 from profanity_filter import *
 import requests
-import os
+import sys
 import json
 
 REDIS_HOST = os.getenv( 'REDIS_PORT_6379_TCP_ADDR', 'localhost' )+':'+os.getenv( 'REDIS_PORT_6379_TCP_PORT', '6379' )
@@ -59,9 +59,12 @@ def update_remote_petition(results):
 
     url = os.environ['PETITIONS_SERVER_URL']
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    r = requests.put(url, data=json.dumps(petition), headers=headers)
-    print 'Updating:'
-    print json.dumps(petition, encoding='utf-8', ensure_ascii=False)
-    print 'PUT request to', url, ' was ', r.status_code
-
+    try:
+        r = requests.put(url, data=json.dumps(petition), headers=headers)
+        print 'Updating:'
+        print json.dumps(petition, encoding='utf-8', ensure_ascii=False)
+        print 'PUT request to', url, ' was ', r.status_code
+    except requests.exceptions.RequestException as e:
+        print e
+        sys.exit(1)
 
