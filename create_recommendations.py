4690344d03c1bf0cb968b8815aab4a79b24a2fd7
Jorge Martinez Ortega <j.martinezortega@gmail.com>
2015-08-27 20:51:37 -0500

    Adds a file contining a second index for recommendatiosn. Adds the ability to create more than one index in elastic search

--- create_recommendations.py
+++ create_recommendations.py
@@ -6,11 +6,19 @@ from elasticsearch import Elasticsearch,helpers
 ES_HOST = os.getenv( 'ELASTICSEARCH_PORT_9200_TCP_ADDR', 'localhost' )+':'+os.getenv( 'ELASTICSEARCH_PORT_9200_TCP_PORT', '9200' )
 es = Elasticsearch([ES_HOST])
 es.indices.delete(index='peticion', ignore=[400, 404])
+null = None
 
-dicts_from_file = []
-with open('peticion_titulo.json','r') as inf:
-    for line in inf:
-        dicts_from_file.append(eval(line))
-
-es.indices.create('peticion')
-helpers.bulk(es,dicts_from_file)
+index_names = [
+    {'name': 'peticion', 'file': 'peticion_titulo.json'},
+    {'name': 'formalities', 'file': 'formalities.json'}
+    ]
+dicts = {}
+for index in index_names:
+    index_name = index['name']
+    dicts[index_name] = []
+    f = index['file']
+    with open(f,'r') as inf:
+        for line in inf:
+            dicts[index_name].append(eval(line))
+    es.indices.create(index_name)
+    helpers.bulk(es,dicts[index_name])
