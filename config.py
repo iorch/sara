118955d4f3f5bb5ea969094ca7edd375a5f27755
iorch <j.martinezortega@gmail.com>
2016-02-23 00:30:13 -0600

    logging reveiwed, config class for staging added

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 import os
+import logging
 
-
-class Config(object):
+class BaseConfig(object):
     DEBUG = os.getenv('DEBUG_MODE')
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_URL')
+    LOGLEVEL = logging.DEBUG
+    LOGFILE = '/logs/sara.log'
+
+
+
+class TestConfig(BaseConfig):
+    LOGLEVEL = logging.DEBUG
+
+class StagingConfig(BaseConfig):
+    LOGFILE = '/logs/stagingapp.log'
+
